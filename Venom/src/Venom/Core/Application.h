d55653ff008c3ff16ece8d3427be23b613114085
nccvector <fzn1020@gmail.com>
2021-06-19 23:14:58 +0500

    Initial Setup

---
+++ Venom/src/Venom/Core/Application.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Core.h"
+
+namespace Venom
+{
+  class Application
+  {
+  public:
+    Application();
+    virtual ~Application();
+
+    void Run();
+  };
+} // namespace Venom
