70bb26b3ad1167d839b4f40e1ddb8c3efa4ce7d2
nccvector <fzn1020@gmail.com>
2021-06-20 03:44:44 +0500

    Initialized Loggin class

---
+++ Venom/src/Venom/Core/Log.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <memory.h>
+#include <spdlog/spdlog.h>
+
+#include "Core.h"
+
+namespace Venom
+{
+  class Log
+  {
+  public:
+    static void Init();
+
+    inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+    inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+
+  private:
+    static std::shared_ptr<spdlog::logger> s_CoreLogger;
+    static std::shared_ptr<spdlog::logger> s_ClientLogger;
+  };
+}
