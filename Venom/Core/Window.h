6e5593b312b134c6ac21b231cf868f98059050c2
nccvector <fzn1020@gmail.com>
2021-06-24 22:38:04 +0500

    Successful Window Launch

--- Venom/Core/Window.h
+++ Venom/Core/Window.h
@@ -3,15 +3,58 @@
 #include <GLFW/glfw3.h>
 #include <string>
 
-namespace Venom {
+#include "Log.h"
 
-	class Window
-	{
-	public:
-    static Window* Get();
-    static void Run();
-    static void Init();
-    static void Loop();
+namespace Venom
+{
+  // Namespace functions
+  void error_callback(int code, const char *description)
+  {
+    // TODO Implement logging here
+    throw std::invalid_argument(description);
+  }
+
+  class Window
+  {
+  public:
+    static Window *Get()
+    {
+      if (window == NULL)
+      {
+        // Creating an instance if null
+        window = new Window();
+      }
+
+      return window;
+    }
+
+    static void Run()
+    {
+
+      Window::Init();
+      Window::Loop();
+    }
+
+    static void Init()
+    {
+      // Attaching error callback for logging
+      glfwSetErrorCallback(error_callback);
+
+      // Initialize glfw
+      if (!glfwInit())
+      {
+        // Implement logging here
+        throw std::invalid_argument("Cannot initialize glfw");
+      }
+
+      // Configure glfw
+      glfwDefaultWindowHints();
+      glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+    }
+
+    static void Loop()
+    {
+    }
 
   private:
     unsigned int Width;
@@ -19,10 +62,18 @@ namespace Venom {
     std::string Title;
 
     // Window pointer
-    Window* window = NULL;
+    static Window *window;
 
     // Constructor
-    Window ();
-	};
+    Window()
+    {
+      Window::Width = 640;
+      Window::Height = 480;
+      Window::Title = "My Application";
+
+      // Must start with NULL
+      Window::window = NULL;
+    }
+  };
 
 }
