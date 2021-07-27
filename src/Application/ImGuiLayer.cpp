053881174d780b85d0fc3cdcf07c3dd08e4cbbcf
nccvector <fzn1020@gmail.com>
2021-07-27 23:13:45 +0500

    ImGui Init moved to its own layer

--- src/Application/ImGuiLayer.cpp
+++ src/Application/ImGuiLayer.cpp
@@ -15,7 +15,20 @@ ImGuiApplication::ImGuiApplication(const std::string &title,
       Vector2{windowSize()} / dpiScaling(), windowSize(), framebufferSize());
 
   ImGuiIO &io = ImGui::GetIO();
-  // ImGui::StyleColorsDark(); // Being set by a function below
+
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
+  //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+  io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
+  // io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+  //io.ConfigFlags |= ImGuiConfigFlags_ViewportsNoTaskBarIcons;
+  //io.ConfigFlags |= ImGuiConfigFlags_ViewportsNoMerge;
+
+  // SETTING DEFAULT FONT
+  io.Fonts->AddFontFromFileTTF("../assets/fonts/OpenSans-Regular.ttf", 18.0f);
+  io.FontDefault = io.Fonts->Fonts[1];
+  m_ImGuiContext.relayout(Vector2{ windowSize() } / dpiScaling(),
+                                               windowSize(), framebufferSize());
+  ImGui::StyleColorsDark(); // Being set by a function below
 
   // When viewports are enabled we tweak WindowRounding/WindowBg so platform
   // windows can look identical to regular ones.
