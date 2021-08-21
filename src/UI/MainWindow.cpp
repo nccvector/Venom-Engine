179aa854740593ec9b396e479a00935164f91adf
nccvector <fzn1020@gmail.com>
2021-08-21 12:44:11 +0000

    opengl QSurfaceFormat

--- src/UI/MainWindow.cpp
+++ src/UI/MainWindow.cpp
@@ -11,9 +11,18 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    // Inserting the opengl widget
+    // Creating the OpenGL widget
     MyOpenGLWidget *openglWidget = new MyOpenGLWidget(ui->centralwidget);
+    // Adding the widget to glContainer (layout)
     ui->glContainer->addWidget(openglWidget);
+
+    // Un-comment this if there are problems with opengl (double check the opengl 
+    // linking in CMakeLists as well)
+    // // Setting up the OpenGL Widget by applying surface format
+    // // Need to apply format before calling show() method
+    // QSurfaceFormat format;
+    // format.setRenderableType(QSurfaceFormat::OpenGL);
+    // openglWidget->setFormat(format);
 
     // Loading stylesheet
     QFile file("stylesheet.qss");
