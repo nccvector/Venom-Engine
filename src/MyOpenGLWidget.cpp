2833c7b67d156924a2aa5b0d5dd2ee78557fd1d0
nccvector <fzn1020@gmail.com>
2021-08-23 19:37:56 +0000

    effectiveWinId to winId

--- src/MyOpenGLWidget.cpp
+++ src/MyOpenGLWidget.cpp
@@ -4,7 +4,7 @@
 #include <QMatrix4x4>
 #include <QOpenGLFunctions>
 
-#include "shaders03.h"
+// #include "shaders03.h"
 
 MyOpenGLWidget::MyOpenGLWidget(QWidget* parent) : QOpenGLWidget(parent)
 {
@@ -12,27 +12,12 @@ MyOpenGLWidget::MyOpenGLWidget(QWidget* parent) : QOpenGLWidget(parent)
 
 MyOpenGLWidget::~MyOpenGLWidget()
 {
-    delete m_program;
-    glDeleteBuffers(1, &m_vbo_triangle);
-    glDeleteBuffers(1, &m_vbo_triangle_colors);
+
 }
 
 void MyOpenGLWidget::initializeGL()
 {
-    initializeOpenGLFunctions();
-    m_program = new QOpenGLShaderProgram(this);
-    m_program->addShaderFromSourceCode(QOpenGLShader::Vertex, vs_source);
-    m_program->addShaderFromSourceCode(QOpenGLShader::Fragment, fs_source);
-    m_program->link();
-    m_attribute_coord2d = m_program->attributeLocation("coord2d");
-    m_attribute_v_color = m_program->attributeLocation("v_color");
-    // VBO Handling
-    glGenBuffers(1, &m_vbo_triangle);
-    glBindBuffer(GL_ARRAY_BUFFER, m_vbo_triangle);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_vertices), triangle_vertices, GL_STATIC_DRAW);
-    glGenBuffers(1, &m_vbo_triangle_colors);
-    glBindBuffer(GL_ARRAY_BUFFER, m_vbo_triangle_colors);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_colors), triangle_colors, GL_STATIC_DRAW);
+
 }
 
 void MyOpenGLWidget::resizeGL(int width, int height)
@@ -44,20 +29,6 @@ void MyOpenGLWidget::resizeGL(int width, int height)
 
 void MyOpenGLWidget::paintGL()
 {
-//    glClearColor(.4f, .7f, .1f, 0.5f);
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    m_program->bind();
-    glEnableVertexAttribArray(m_attribute_v_color);
-    glBindBuffer(GL_ARRAY_BUFFER, m_vbo_triangle_colors);
-    glVertexAttribPointer(m_attribute_v_color, 3, GL_FLOAT, GL_FALSE, 0, 0);
-    glEnableVertexAttribArray(m_attribute_coord2d);
-    glBindBuffer(GL_ARRAY_BUFFER, m_vbo_triangle);
-    glVertexAttribPointer(m_attribute_coord2d, 2, GL_FLOAT, GL_FALSE, 0, 0);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-    glDisableVertexAttribArray(m_attribute_coord2d);
-    glDisableVertexAttribArray(m_attribute_v_color);
-    m_program->release();
 }
 
