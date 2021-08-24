16c7c24ec850a936e22a15a4b0a50b7bc7a22bcb
nccvector <fzn1020@gmail.com>
2021-08-24 15:11:38 +0000

    Panda window resizing

--- src/MyOpenGLWidget.h
+++ src/MyOpenGLWidget.h
@@ -1,6 +1,12 @@
 #ifndef WINDOW_H
 #define WINDOW_H
 
+#include "pandaFramework.h"
+#include "pandaSystem.h"
+
+#include "genericAsyncTask.h"
+#include "asyncTaskManager.h"
+
 #include <QOpenGLWidget>
 #include <QOpenGLFunctions>
 #include <QOpenGLShaderProgram>
@@ -17,10 +23,10 @@ protected:
     void resizeGL(int width, int height) Q_DECL_OVERRIDE;
     void paintGL() Q_DECL_OVERRIDE;
 
-private:
-    QOpenGLShaderProgram *m_program;
-    GLint m_attribute_coord2d, m_attribute_v_color;
-    GLuint m_vbo_triangle, m_vbo_triangle_colors;
+public:
+    PandaFramework *framework;
+    WindowProperties prop;
+    WindowFramework *window;
 };
 
 #endif //WINDOW_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,3 @@
-#include "pandaFramework.h"
-#include "pandaSystem.h"
-
-#include "genericAsyncTask.h"
-#include "asyncTaskManager.h"
-
 #include "MainWindow.h"
 
 #include <QApplication>
@@ -16,43 +10,13 @@ int main(int argc, char *argv[])
 
     w.show();
 
-    QWidget *qw = w.openglWidget;
-
-    PandaFramework framework;
-    framework.open_framework();
-
-    WindowProperties prop;
-    framework.get_default_window_props(prop);
-
-    // Snapping the panda display to top-left of parent widget
-    LPoint2i p2i(0, 0);
-    prop.set_origin(p2i);
-
-    // Resize needs to be either called inside the "resize" method of custom widget
-    // or can be binded with the resize function of the parent widget
-
-    prop.set_parent_window((size_t)qw->winId());
-
-    // cout<<qw->winId()<<endl;
-
-    WindowFramework *window = framework.open_window(prop,0);
-    window->setup_trackball();
-
-    // Load the environment model.
-    NodePath scene = window->load_model(framework.get_models(), "models/environment");
-    // Reparent the model to render.
-    scene.reparent_to(window->get_render());
-    // Apply scale and position transforms to the model.
-    scene.set_scale(0.25f, 0.25f, 0.25f);
-    scene.set_pos(-8, 42, 0);
-
     // this is the main loop
     Thread *current_thread = Thread::get_current_thread();
-    while (framework.do_frame(current_thread)) {
+    while (w.openglWidget->framework->do_frame(current_thread)) {
         a.processEvents();
     }
 
-    framework.close_framework();
+    w.openglWidget->framework->close_framework();
 
     current_thread->prepare_for_exit();
 
