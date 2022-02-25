438cc118385367adbd647acd2bbe8817981e0464
nccvector <fzn1020@gmail.com>
2022-02-26 02:41:06 +0500

    raycasting

--- States/PickState.cpp
+++ States/PickState.cpp
@@ -17,8 +17,35 @@ void PickState::Enter()
 bool PickState::mousePressed(const MouseButtonEvent& evt)
 {
     // Cast a ray into the scene every time left click is pressed
-    Ogre::Ray ray;
-    Application::Singleton().camera->getCameraToViewportRay(evt.x, evt.y, &ray);
+    Ogre::Ray ray(
+        Ogre::Vector3(0, 0, 0),
+        Ogre::Vector3(0, 0 ,0)
+    );
+
+    // Getting the application handle
+    Application& app = Application::Singleton();
+
+    // Getting the camera to world ray
+    // coords must be uv's [0 to 1]
+    app.camera->getCameraToViewportRay(
+        evt.x / float(app.getRenderWindow()->getWidth()), 
+        evt.y / float(app.getRenderWindow()->getHeight()), 
+        &ray);
+
+    // Assinging the query ray
+    app.raySceneQuery->setRay(ray);
+
+    // Check the intersections
+    if(app.raySceneQuery->execute().size() > 0)
+    {
+        Console::getSingleton().AddLog("HIT!");
+        // Finding the closest object from query result
+        app.raySceneQueryResult = &app.raySceneQuery->getLastResults();
+
+        // Do something with the results
+        // Enable the bounding volume of the object
+        (*app.raySceneQueryResult)[0].movable->getParentSceneNode()->showBoundingBox(true);
+    }
 
     std::string msg = "PickState::" + std::to_string(evt.x);
     Console::getSingleton().AddLog(&msg[0]);
