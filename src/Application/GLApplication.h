447a6465617ef3d5193a9de07b7785b0866c8358
nccvector <fzn1020@gmail.com>
2022-03-06 13:00:43 +0500

    smol changes

--- src/Application/GLApplication.h
+++ src/Application/GLApplication.h
@@ -1,19 +1,3 @@
-/**
- * Copyright 2020 Nghia Truong <nghiatruong.vn@gmail.com>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 #pragma once
 
 #include "Camera/ArcBallCamera.h"
@@ -40,7 +24,7 @@ using Scene3D  = SceneGraph::Scene<SceneGraph::MatrixTransformation3D>;
 class GLApplication : public Platform::Application {
 public:
     explicit GLApplication(const std::string& title, const Arguments& arguments,
-                           const Vector2i& defaultWindowSize = Vector2i{ 1920, 1080 });
+                           const Vector2i& defaultWindowSize = Vector2i{ 1280, 720 });
     virtual ~GLApplication() {}
 
 protected:
